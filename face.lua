881f6107676385fb19d0e1fa16cce97c6f52d697
charlie-wt <cwt1g14@soton.ac.uk>
2024-01-27 23:07:45 +0000

    drawing eyes

--- face.lua
+++ face.lua
@@ -1,6 +1,8 @@
 sprite_info = {
 	priest = {
 		collar_sprite = 101,
+		neutral_eyes_sprite = 80,
+		neutral_mouth_sprite = 83,
 	}
 }
 
@@ -83,27 +85,37 @@ function draw_ear_right(x, y)
 	                           10, 0, true)
 end
 
-function draw_collar_left(x, y)
-	spr(101, x, y, 5, 2, false, false)
-end
-
-function draw_collar_right(x, y)
-	spr(101, x, y, 5, 2, true, false)
-end
-
 function tiles(x)
 	return x * 8
 end
 
+function draw_collar_left(sprite_idx, x, y)
+	spr(sprite_idx, x, y, 5, 2, false, false)
+end
+function draw_collar_right(sprite_idx, x, y)
+	spr(sprite_idx, x, y, 5, 2, true, false)
+end
 function draw_collar(sprite_idx, head_bottom)
-	draw_collar_left(64 - tiles(5), head_bottom - tiles(1))
-	draw_collar_right(64 - 1,       head_bottom - tiles(1))
+	draw_collar_left(sprite_idx, 64 - tiles(5), head_bottom - tiles(1))
+	draw_collar_right(sprite_idx, 64 - 1,       head_bottom - tiles(1))
+end
+
+function draw_neutral_eyes_left(sprite_idx, x, y)
+	spr(sprite_idx, x, y, 3, 3, true, false)
+end
+function draw_neutral_eyes_right(sprite_idx, x, y)
+	spr(sprite_idx, x, y, 3, 3, false, false)
+end
+function draw_neutral_eyes(sprite_idx, head_top, head_left, head_right)
+	draw_neutral_eyes_left(sprite_idx, head_left + 3, head_top + tiles(3))
+	draw_neutral_eyes_right(sprite_idx, head_right - tiles(3) - 3, head_top + tiles(3))
 end
 
 function draw_head(name)
 	-- TODO #temp
 	name = "priest"
 
+	-- setup
 	pal()
 	palt(0, false)
 	palt(13, true)
@@ -113,11 +125,18 @@ function draw_head(name)
 	local head_top = 1
 	local head_bottom = head_top + tiles(10)
 
+	-- base
 	draw_base_left(head_left, head_top)
 	draw_base_right(head_right - tiles(4), head_top)
 
 	draw_ear_left(head_left - tiles(1) + 1, head_top + tiles(3))
 	draw_ear_right(head_right - 1,          head_top + tiles(3))
 
+	-- features
 	draw_collar(sprite_info[name].collar_sprite, head_bottom)
+
+	draw_neutral_eyes(sprite_info[name].neutral_eyes_sprite,
+	                  head_top, head_left, head_right)
+
+	-- 
 end
