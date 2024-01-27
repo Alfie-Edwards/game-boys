2dc96299679b0566350ee2aa294fe3dfd76d4ca8
charlie-wt <cwt1g14@soton.ac.uk>
2024-01-27 18:43:36 +0000

    more people

--- people.lua
+++ people.lua
@@ -76,6 +76,83 @@ people = {
 		acceptance_text = "a thousand blessings!! now i can terrify everyone at the rectory",
 		rejection_text = "god tests us all child, and we cannot always succeed",
 	},
+	{
+		name = "diver",
+		face = 0,
+		skin_tone = 1,
+		initial_laugh = {
+			speed = 1,
+			pitch = 1,
+			fun = 2,
+			length = 0,
+		},
+		desired_laugh = {
+			speed = 2,
+			pitch = 2,
+			fun = 0,
+			length = 0,
+		},
+		initial_prompt = {
+			"hey there! you're the first human i've seen in months, hahaha",
+			"sure is dry up here",
+			"i need a laugh for a dolphin comedy show, dolphin comedy is no laughing matter!"},
+		adjustment_prompts = {
+			pitch = {
+				more = "i'm not going to a show for whales!"
+			},
+			speed = {
+				more = "dontcha think that sounds more like a fire alarm than a dolphin?"
+			},
+			length = {
+				less = "this feels too wild. i'm just the audience, it's not about me"
+			},
+			fun = {
+				less = "please. this is very important to me. not some joke"
+			},
+		},
+		acceptance_text = "that's amazing! I'll fit right in", "they'll never call me land lubber again",
+		rejection_text = "no, no... this won't do at all...", "well, i guess there's always catfish cabaret",
+	},
+	{
+		name = "bobo the clown",
+		face = 0,
+		skin_tone = 1,
+		initial_laugh = {
+			speed = 2,
+			pitch = 1,
+			fun = 2,
+			length = 2,
+		},
+		desired_laugh = {
+			speed = 1,
+			pitch = 1,
+			fun = 1,
+			length = 1,
+		},
+		initial_prompt = {
+			"hey hey! i'm bobo, world famous entertainer and fun for all the family!",
+			"no one takes me seriously! i need a sensible, reasonable laugh. like goldilocks!"},
+		adjustment_prompts = {
+			pitch = {
+				less = "no no, that's squeakier than my nose!",
+				more = "i always felt more like a tenor than a baritone",
+			},
+			speed = {
+				less = "is that rushing? it sounds like rushing to me, don't rush my laugh",
+				more = "is that dragging? i think it was dragging, don't drag my laugh",
+			},
+			length = {
+				less = "wow, that really goes on huh. on and on... just... keeps... going...",
+				more = "not much of a laugh, is it. more like a bark. could i have some more?",
+			},
+			fun = {
+				less = "i want to laugh at the joke. not be the joke.",
+				more = "it's a laugh. it's gotta be a little jovial!",
+			},
+		},
+		acceptance_text = "finally! now i can do what i've always wanted", "be an undertaker, like my father",
+		rejection_text = "well, i guess everyone will keep laughing at ol' bobo...",
+	},
 	-- {
 	-- 	name = "bob",
 	-- 	face = 0,
