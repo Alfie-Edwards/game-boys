dac2adc5162c2de868ebcab79a9f26cbe2db63c4
charlie-wt <cwt1g14@soton.ac.uk>
2024-01-28 13:47:57 +0000

    make all the sprite info functions

--- face.lua
+++ face.lua
@@ -1,30 +1,154 @@
 sprites = {
-	neutral_eyes = 80,
-	neutral_mouth = 83,
-	base = {0, 0},
-	ear = {10, 0},
+	neutral_eyes =
+		function(x_left, x_right, y)
+			spr(80, x_left,  y, 3, 3, true,  false)
+			spr(80, x_right, y, 3, 3, false, false)
+		end,
+	neutral_mouth =
+		function(x, y)
+			spr(83, x, y, 2, 3, false, false)
+			spr(83, x + tiles(2) - 1, y, 2, 3, true, false)
+		end,
+	base =
+		function(x, y)
+			draw_rotated_anticlockwise(x, y,
+			                           10, 4,
+			                           0, 0)
+			draw_rotated_anticlockwise(x + tiles(4) - 1, y,
+			                           10, 4,
+			                           0, 0, true)
+		end,
+	ears =
+		function(x_left, x_right, y)
+			draw_rotated_anticlockwise(x_left, y,
+			                           3, 1,
+			                           10, 0)
+			draw_rotated_anticlockwise(x_right, y,
+			                           3, 1,
+			                           10, 0, true)
+		end,
 	beardy = {
-		beard = 139,
-		collar = 122,
-		hair = 74,
-		hair_edge = {10, 1},
-		eyes = 206,
-		mouth = 93,
+		beard =
+			function(x, y)
+				spr(139, x, y, 5, 4, false, false)
+				spr(139, x + tiles(5) - 1, y, 5, 4, true, false)
+			end,
+		collar =
+			function(x, y)
+				spr(122, x, y, 5, 1, false, false)
+				spr(122, x + tiles(5), y, 5, 1, true, false)
+			end,
+		hair =
+			function(x, y)
+				spr(74, x, y, 3, 3, false, false)
+				spr(74, x + tiles(3), y, 3, 3, true, false)
+			end,
+		hair_edge =
+			function(x_left, x_right, y)
+				draw_rotated_anticlockwise(x_left,  y, 3, 2, 10, 1)
+				draw_rotated_anticlockwise(x_right, y, 3, 2, 10, 1, true)
+			end,
+		eyes =
+			function(x_left, x_right, y)
+				spr(206, x_left,  y, 2, 3, false, false)
+				spr(206, x_right, y, 2, 3, true, false)
+			end,
+		mouth =
+			function(x, y)
+				spr(93, x, y, 3, 2, false, false)
+			end,
 	},
 	priest = {
-		collar = 101,
-		eyes = 26,
-		mouth = 29,
+		collar =
+			function(x, y)
+				spr(101, x, y, 5, 2, false, false)
+				spr(101, x + tiles(5), y, 5, 2, true, false)
+			end,
+		eyes =
+			function(x_left, x_right, y)
+				spr(26, x_left, y, 3, 3, true, false)
+				spr(26, x_right, y, 3, 3, false, false)
+			end,
+		mouth =
+			function(x, y)
+				spr(29, x, y, 3, 3, false, false)
+			end,
 	},
 	diver = {
-		helmet = {13, 0},
-		helmet_side = {13, 4},
-		bubbles_1 = 64,
-		bubbles_2 = 65,
-		bubbles_3 = 224,
-		bubbles_4 = 226,
+		helmet =
+			function(x, y)
+				draw_rotated_anticlockwise(x, y,
+				                           11, 4,
+				                           13, 0)
+				draw_rotated_anticlockwise(x + tiles(4), y,
+				                           11, 4,
+				                           13, 0,
+				                           true)
+			end,
+		helmet_side =
+			function(x_left, x_right, y)
+				draw_rotated_anticlockwise(x_left, y,
+				                           6, 2,
+				                           13, 4)
+				draw_rotated_anticlockwise(x_right, y,
+				                           6, 2,
+				                           13, 4,
+				                           true)
+			end,
+		bubbles = {
+			function (x, y)
+				palt(12, false)
+				palt(1, true)
+				spr(64, x, y, 1, 1, false, false)
+				palt(12, false)
+				palt(1, false)
+			end,
+			function (x, y)
+				palt(12, false)
+				palt(1, true)
+				spr(65, x, y, 1, 1, false, false)
+				palt(12, false)
+				palt(1, false)
+			end,
+			function (x, y)
+				palt(12, false)
+				palt(1, true)
+				spr(224, x, y - tiles(1), 2, 2, false, false)
+				palt(12, false)
+				palt(1, false)
+			end,
+			function (x, y)
+				palt(12, true)
+				palt(1, false)
+				pal(1, 12)
+				spr(224, x, y - tiles(1), 2, 2, false, false)
+				pal(1)
+				palt(12, false)
+				palt(1, false)
+			end,
+			function (x, y)
+				palt(12, false)
+				palt(1, true)
+				spr(226, x, y - tiles(1), 2, 2, false, false)
+				palt(12, false)
+				palt(1, false)
+			end,
+			function (x, y)
+				palt(12, true)
+				palt(1, false)
+				pal(1, 12)
+				spr(64, x, y, 1, 1, false, false)
+				pal(1)
+				palt(12, false)
+				palt(1, false)
+			end,
+		},
 	}
 }
+
+function tiles(x)
+	return x * 8
+end
 
 function draw_rotated_anticlockwise(x, y, w_tiles, h_tiles, map_x, map_y, flip_y)
 	if (flip_y == nil) flip_y = false
@@ -58,253 +182,85 @@ function draw_rotated_clockwise(x, y, w_tiles, h_tiles, map_x, map_y, flip_y)
 	end
 end
 
-function draw_base_left(x, y)
-	draw_rotated_anticlockwise(x, y,
-	                           10, 4,
-	                           sprites.base[1], sprites.base[2])
-end
 
-function draw_base_right(x, y)
-	draw_rotated_anticlockwise(x, y,
-	                           10, 4,
-	                           sprites.base[1], sprites.base[2], true)
-end
-
-function draw_ear_left(x, y)
-	draw_rotated_anticlockwise(x, y,
-	                           3, 1,
-	                           sprites.ear[1], sprites.ear[2])
-end
-
-function draw_ear_right(x, y)
-	draw_rotated_anticlockwise(x, y,
-	                           3, 1,
-	                           sprites.ear[1], sprites.ear[2], true)
-end
-
-function tiles(x)
-	return x * 8
+function draw_ears(head_left, head_right, head_top)
+	sprites.ears(head_left - tiles(1) + 1, head_right - 1,
+	             head_top + tiles(3))
 end
 
 function draw_neutral_eyes(head_top, head_left, head_right)
-	function draw_neutral_eyes_left(x, y)
-		spr(sprites.neutral_eyes, x, y, 3, 3, true, false)
-	end
-	function draw_neutral_eyes_right(x, y)
-		spr(sprites.neutral_eyes, x, y, 3, 3, false, false)
-	end
-
-	draw_neutral_eyes_left(head_left + 3,              head_top + tiles(3))
-	draw_neutral_eyes_right(head_right - tiles(3) - 3, head_top + tiles(3))
+	sprites.neutral_eyes(
+		head_left + 3, head_right - tiles(3) - 3,
+		head_top + tiles(3))
 end
 
 function draw_neutral_mouth(head_bottom, head_left, head_right)
-	function draw_neutral_mouth_left(x, y)
-		spr(sprites.neutral_mouth, x, y, 2, 3, false, false)
-	end
-	function draw_neutral_mouth_right(x, y)
-		spr(sprites.neutral_mouth, x, y, 2, 3, true, false)
-	end
-
-	draw_neutral_mouth_left(head_left + tiles(2),   head_bottom - tiles(4))
-	draw_neutral_mouth_right(head_right - tiles(4), head_bottom - tiles(4))
+	sprites.neutral_mouth(head_left + tiles(2), head_bottom - tiles(4))
 end
 
 function draw_priest(emotion, head_left, head_right, head_top, head_bottom)
-	function draw_collar_left(x, y)
-		spr(sprites.priest.collar, x, y, 5, 2, false, false)
-	end
-	function draw_collar_right(x, y)
-		spr(sprites.priest.collar, x, y, 5, 2, true, false)
-	end
-	function draw_priest_laughing_eyes_left(x, y)
-		spr(sprites.priest.eyes, x, y, 3, 3, true, false)
-	end
-	function draw_priest_laughing_eyes_right(x, y)
-		spr(sprites.priest.eyes, x, y, 3, 3, false, false)
-	end
-	function draw_priest_laughing_mouth(x, y)
-		spr(sprites.priest.mouth, x, y, 3, 3, false, false)
-	end
-
 	-- base layer
-	draw_base_left(head_left,              head_top)
-	draw_base_right(head_right - tiles(4), head_top)
-	draw_ear_left(head_left - tiles(1) + 1, head_top + tiles(3))
-	draw_ear_right(head_right - 1,          head_top + tiles(3))
+	sprites.base(head_left, head_top)
+	draw_ears(head_left, head_right, head_top)
 
 	if emotion == "laughing" then
-		draw_priest_laughing_eyes_left(head_left + 4, head_top + tiles(3))
-		draw_priest_laughing_eyes_right(head_right - tiles(3) - 4, head_top + tiles(3))
-		draw_priest_laughing_mouth(64 - tiles(1.5) - 1, head_bottom - tiles(4) - 1)
+		sprites.priest.eyes(head_left + 4, head_right - tiles(3) - 4,
+		                    head_top + tiles(3))
+		sprites.priest.mouth(head_left + tiles(2.5) - 1, head_bottom - tiles(4) - 1)
 	else
 		draw_neutral_eyes(head_top, head_left, head_right)
 		draw_neutral_mouth(head_bottom, head_left, head_right)
 	end
 
 	-- features
-	draw_collar_left(64 - tiles(5),  head_bottom - tiles(1))
-	draw_collar_right(64 - 1,        head_bottom - tiles(1))
+	sprites.priest.collar(head_left - tiles(1), head_bottom - tiles(1))
 end
 
 function draw_big_beardy_man(emotion, head_left, head_right, head_top, head_bottom)
-	function draw_collar_left(x, y)
-		spr(sprites.beardy.collar, x, y, 5, 1, false, false)
-	end
-	function draw_collar_right(x, y)
-		spr(sprites.beardy.collar, x, y, 5, 1, true, false)
-	end
-	function draw_beard_left(x, y)
-		spr(sprites.beardy.beard, x, y, 5, 4, false, false)
-	end
-	function draw_beard_right(x, y)
-		spr(sprites.beardy.beard, x, y, 5, 4, true, false)
-	end
-	function draw_hair_left(x, y)
-		spr(sprites.beardy.hair, x, y, 3, 3, false, false)
-	end
-	function draw_hair_right(x, y)
-		spr(sprites.beardy.hair, x, y, 3, 3, true, false)
-	end
-	function draw_hair_side_left(x, y)
-		draw_rotated_anticlockwise(x, y, 3, 2, sprites.beardy.hair_edge[1],
-		                                       sprites.beardy.hair_edge[2])
-	end
-	function draw_hair_side_right(x, y)
-		draw_rotated_anticlockwise(x, y, 3, 2,
-		                           sprites.beardy.hair_edge[1],
-		                           sprites.beardy.hair_edge[2],
-		                           true)
-	end
-	function draw_big_beardy_man_eyes_left(x, y)
-		spr(sprites.beardy.eyes, x, y, 2, 3, true, false)
-	end
-	function draw_big_beardy_man_eyes_right(x, y)
-		spr(sprites.beardy.eyes, x, y, 2, 3, false, false)
-	end
-	function draw_big_beardy_man_eyes_mouth(x, y)
-		spr(sprites.beardy.mouth, x, y, 3, 2, false, false)
-	end
-
 	-- base layer
-	draw_base_left(head_left,              head_top)
-	draw_base_right(head_right - tiles(4), head_top)
-	draw_ear_left(head_left - tiles(1) + 1, head_top + tiles(3))
-	draw_ear_right(head_right - 1,          head_top + tiles(3))
+	sprites.base(head_left, head_top)
+	draw_ears(head_left, head_right, head_top)
 
 	if emotion == "neutral" then
 		draw_neutral_eyes(head_top, head_left, head_right)
 	else
-		draw_big_beardy_man_eyes_left(head_left + tiles(2) - 5,               head_top + tiles(3))
-		draw_big_beardy_man_eyes_right(head_right - tiles(3) - 3, head_top + tiles(3))
+		sprites.beardy.eyes(
+			head_left + tiles(2) - 5,
+			head_right - tiles(3) - 3,
+			head_top + tiles(3))
 	end
 
 	-- features
-	draw_collar_left(64 - tiles(5), head_bottom)
-	draw_collar_right(64 - 1,       head_bottom)
-	draw_beard_left(64 - tiles(5),      head_bottom - tiles(4))
-	draw_beard_right(64 - 1,            head_bottom - tiles(4))
-	draw_hair_left(64 - tiles(3),       head_top - 1)
-	draw_hair_right(64 - 1,             head_top - 1)
-	draw_hair_side_left(head_left - tiles(1) + 1,   head_top + tiles(1))
-	draw_hair_side_right(head_right - tiles(1) - 1, head_top + tiles(1))
+	sprites.beardy.collar(head_left - tiles(1), head_bottom)
+	sprites.beardy.beard(64 - tiles(5), head_bottom - tiles(4))
+	sprites.beardy.hair(64 - tiles(3), head_top - 1)
+	sprites.beardy.hair_edge(head_left - tiles(1) + 1, head_right - tiles(1) - 1,
+	                         head_top + tiles(1))
 
 	if emotion ~= "neutral" then
-		draw_big_beardy_man_eyes_mouth(64 - tiles(1.5), head_bottom - tiles(3))
+		sprites.beardy.mouth(64 - tiles(1.5), head_bottom - tiles(3))
 	end
 end
 
 function draw_diver(emotion, head_left, head_right, head_top, head_bottom)
-	function draw_helmet_left(x, y)
-		draw_rotated_anticlockwise(x, y,
-		                           11, 4,
-		                           sprites.diver.helmet[1], sprites.diver.helmet[2])
-	end
-	function draw_helmet_right(x, y)
-		draw_rotated_anticlockwise(x, y,
-		                           11, 4,
-		                           sprites.diver.helmet[1], sprites.diver.helmet[2],
-		                           true)
-	end
-	function draw_side_left(x, y)
-		draw_rotated_anticlockwise(x, y,
-		                           6, 2,
-		                           sprites.diver.helmet_side[1], sprites.diver.helmet_side[2])
-	end
-	function draw_side_right(x, y)
-		draw_rotated_anticlockwise(x, y,
-		                           6, 2,
-		                           sprites.diver.helmet_side[1], sprites.diver.helmet_side[2],
-		                           true)
-	end
-
-	draw_helmet_left(head_left, head_top)
-	draw_helmet_right(head_right - tiles(4), head_top)
-	draw_side_left(head_left - tiles(2) + 1, head_top + tiles(2))
-	draw_side_right(head_right - 1,          head_top + tiles(2))
+	sprites.diver.helmet(head_left, head_top)
+	sprites.diver.helmet_side(
+		head_left - tiles(2) + 1,
+		head_right,
+		head_top + tiles(2) + 1)
 
 	local bubbles_x = head_left + tiles(3.5)
 	local bubbles_y = head_top
-	local bubbles_stages = {
-		function ()
-			palt(12, false)
-			palt(1, true)
-			spr(sprites.diver.bubbles_1, bubbles_x, bubbles_y, 1, 1, false, false)
-			palt(12, false)
-			palt(1, false)
-		end,
-		function ()
-			palt(12, false)
-			palt(1, true)
-			spr(sprites.diver.bubbles_2, bubbles_x, bubbles_y, 1, 1, false, false)
-			palt(12, false)
-			palt(1, false)
-		end,
-		function ()
-			palt(12, false)
-			palt(1, true)
-			spr(sprites.diver.bubbles_3, bubbles_x, bubbles_y - tiles(1), 2, 2, false, false)
-			palt(12, false)
-			palt(1, false)
-		end,
-		function ()
-			palt(12, true)
-			palt(1, false)
-			pal(1, 12)
-			spr(sprites.diver.bubbles_3, bubbles_x, bubbles_y - tiles(1), 2, 2, false, false)
-			pal(1)
-			palt(12, false)
-			palt(1, false)
-		end,
-		function ()
-			palt(12, false)
-			palt(1, true)
-			spr(sprites.diver.bubbles_4, bubbles_x, bubbles_y - tiles(1), 2, 2, false, false)
-			palt(12, false)
-			palt(1, false)
-		end,
-		function ()
-			palt(12, true)
-			palt(1, false)
-			pal(1, 12)
-			spr(sprites.diver.bubbles_1, bubbles_x, bubbles_y, 1, 1, false, false)
-			pal(1)
-			palt(12, false)
-			palt(1, false)
-		end,
-	}
 
-	local bubbles_stage = flr((t() * 5) % #bubbles_stages) + 1
+	local bubbles_stage = flr((t() * 5) % #sprites.diver.bubbles) + 1
 
-	bubbles_stages[bubbles_stage]()
+	sprites.diver.bubbles[bubbles_stage](bubbles_x, bubbles_y)
 end
 
 function draw_clown(emotion, head_left, head_right, head_top, head_bottom)
 	-- base layer
-	draw_base_left(head_left,              head_top)
-	draw_base_right(head_right - tiles(4), head_top)
-	draw_ear_left(head_left - tiles(1) + 1, head_top + tiles(3))
-	draw_ear_right(head_right - 1,          head_top + tiles(3))
+	sprites.base(head_left, head_top)
+	draw_ears(head_left, head_right, head_top)
 end
 
 function draw_head(name, emotion)
@@ -313,8 +269,8 @@ function draw_head(name, emotion)
 	-- TODO #temp
 	-- name = "big beardy man"
 	-- name = "priest"
-	-- name = "diver"
-	name = "clown"
+	name = "diver"
+	-- name = "clown"
 
 	-- setup
 	pal()
